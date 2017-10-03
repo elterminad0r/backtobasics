0a31c8e44613f7af400e85065a2af50cdcd466ac
Izaak van Dongen <izaak.van.dongen@gmail.com>
2017-10-03 10:15:12 +0100

    coins

---
+++ challenges/coinchange.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+import sys
+
+def make_change(coins, n):
+    lookup = [1] + [0] * n
+    for coin in coins:
+        for i in range(n+1-coin):
+            lookup[i+coin] += lookup[i]
+    return lookup[n]
+    
+n,m = input().strip().split(' ')
+n,m = [int(n),int(m)]
+coins = [int(coins_temp) for coins_temp in input().strip().split(' ')]
+print(make_change(coins, n))
