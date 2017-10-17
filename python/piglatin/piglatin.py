6d3549f6b33ef96d0c30c5518ac4d640587b83dd
Izaak van Dongen <izaak.van.dongen@gmail.com>
2017-10-17 21:27:19 +0100

    more piglatin - sentences

--- python/piglatin/piglatin.py
+++ python/piglatin/piglatin.py
@@ -1,4 +1,5 @@
 import re
+import readline
 
 vowels = set("aeiou")
 
@@ -11,7 +12,27 @@ def piglatin(s):
 def re_latin(s):
     return re.sub(r"([^aeiou]*)(.*)", r"\2\1ay", s)
 
+def sentence_latin(sent):
+    words = sent.split()
+
+    output = [re_latin(word) for word in words]
+
+#    lazy generator which is iterable
+#    output = (re_latin(word) for word in words)
+
+#    output = map(re_latin, words)
+
+#    output = []
+#    for word in words:
+#        output.append(re_latin(word))
+
+    return " ".join(output)
+
+#while True:
+#    word = input("Enter word > ").lower()
+#    print(piglatin(word))
+#    print(re_latin(word))
+
 while True:
-    word = input("Enter word > ").lower()
-    print(piglatin(word))
-    print(re_latin(word))
+    sent = input("enter sentence > ")
+    print(sentence_latin(sent))
