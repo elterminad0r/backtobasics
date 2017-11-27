5f7f618137a1afae11cfae8ef2217ec6aa4766fd
Izaak van Dongen <izaak.van.dongen@gmail.com>
2017-11-27 09:51:09 +0000

    added radix point notation

--- pascal/data_rep/PTwosComplement.pas
+++ pascal/data_rep/PTwosComplement.pas
@@ -1,18 +1,40 @@
+{$MODE OBJFPC}
+
 program PTwosComplement;
 
-uses SysUtils;
+uses SysUtils, Math;
 
 function _from_t_comp(from: string; i: integer): integer;
 begin
-    if i = 1 then
-        _from_t_comp := -1 * strtoint(from[i])
+    if i = 0 then
+        _from_t_comp := 0
+    else if from[i] in ['0', '1'] then
+        if i = 1 then
+            _from_t_comp := -1 * strtoint(from[i])
+        else
+            _from_t_comp := strtoint(from[i]) + 2 * _from_t_comp(from, i - 1)
     else
-        _from_t_comp := strtoint(from[i]) + 2 * _from_t_comp(from, i - 1);
+        _from_t_comp := _from_t_comp(from, i - 1);
 end;
 
-function from_t_comp(from: string): integer;
+function from_t_comp(from: string): real;
 begin
-    from_t_comp := _from_t_comp(from, length(from));
+    if pos('.', from) <> 0 then
+        from_t_comp := _from_t_comp(from, length(from))
+                      * power(2, pos('.', from) - length(from))
+    else
+        from_t_comp := _from_t_comp(from, length(from));
+end;
+
+function _frac_to_binary(from: real): string;
+begin
+    if from = 0.0 then
+        _frac_to_binary := '0'
+    else
+        if from >= 0.5 then
+            _frac_to_binary := '1' + _frac_to_binary(2 * from - 1)
+        else
+            _frac_to_binary := '0' + _frac_to_binary(2 * from);
 end;
 
 function to_binary(from: integer): string;
@@ -23,12 +45,19 @@ begin
         to_binary := to_binary(from div 2) + inttostr(from mod 2);
 end;
 
+function to_binary(from: real): string;
+begin
+    to_binary := to_binary(trunc(from)) + '.' + _frac_to_binary(from - trunc(from));
+end;
+
 function _flip(from: string; i: integer): string;
 begin
     if i = 0 then
         _flip := ''
+    else if from[i] in ['0', '1'] then
+        _flip := _flip(from, i - 1) + inttostr(1 - strtoint(from[i]))
     else
-        _flip := _flip(from, i - 1) + inttostr(1 - strtoint(from[i]));
+        _flip := _flip(from, i - 1) + from[i];
 end;
 
 function _complement(from: string; i: integer): string;
@@ -37,8 +66,10 @@ begin
         _complement := ''
     else if from[i] = '1' then
         _complement := _flip(from, i - 1) + '1'
+    else if from[i] = '0' then
+        _complement := _complement(from, i - 1) + '0'
     else
-        _complement := _complement(from, i - 1) + '0';
+        _complement := _complement(from, i - 1) + from[i];
 end;
 
 function complement(from: string): string;
@@ -48,24 +79,33 @@ end;
 
 function to_t_comp(from: string): string;
 begin
-    if from[1] = '-' then
-        to_t_comp := complement(to_binary(abs(strtoint(from))))
-    else
-        to_t_comp := to_binary(strtoint(from));
+    try
+        if from[1] = '-' then
+            to_t_comp := complement(to_binary(abs(strtofloat(from))))
+        else
+            to_t_comp := to_binary(strtofloat(from));
+    except
+        on EConvertError do
+            to_t_comp := 'invalid float';
+    end;
 end;
 
 procedure menu;
 var
     operation, operand: string;
 begin
-    write('Would you like to convert /from/ or /to/ two''s complement? '); readln(operation);
-    write('What is your operand? '); readln(operand);
-    if operation = 'from' then
-        writeln(from_t_comp(operand))
-    else if operation = 'to' then
-        writeln(to_t_comp(operand))
-    else
-        writeln('unrecognised operation');
+    write('Would you like to convert /from/ or /to/ two''s complement? ');
+    readln(operation);
+    if not ((operation = 'from') or (operation = 'to')) then
+        writeln('unrecognised command')
+    else begin
+        write('What is your operand? '); readln(operand);
+        if operation = 'from' then
+            writeln(FormatFloat('0.##########################################',
+                                from_t_comp(operand)))
+        else if operation = 'to' then
+            writeln(to_t_comp(operand));
+    end;
 end;
 
 begin
