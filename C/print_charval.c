76c40459e290144bb97d5c06fff983cfa4771c87
Izaak van Dongen <izaak.van.dongen@gmail.com>
2017-12-08 10:55:17 +0000

    more ascii programs

---
+++ C/print_charval.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    printf("'A' has ascii value %d\n", 'A');
+    return 0;
+}
+
+// compiled with $ gcc -Wall -pedantic -std=c99 -o bin/print_charval print_charval.c
