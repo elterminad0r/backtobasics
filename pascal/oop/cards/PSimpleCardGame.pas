bd234219dc377f17099082e5a38a48c3e7f0f642
Izaak van Dongen <izaak.van.dongen@gmail.com>
2018-03-06 11:50:06 +0000

    modified card game to deal until pack empty

--- pascal/oop/cards/PSimpleCardGame.pas
+++ pascal/oop/cards/PSimpleCardGame.pas
@@ -8,21 +8,27 @@ var
     card_pack: TPack;
     card_a, card_b: TCard;
     a_score, b_score, i: integer;
+    cards_remain: boolean = true;
+
 begin
     a_score := 0;
     b_score := 0;
 
     card_pack := TPack.create;
-    for i := 0 to 25 do begin
-        card_pack.Deal(card_a);
-        card_pack.Deal(card_b);
-        write(format('cards drawn: A::%20s and B::%20s >> ', [card_a.getname, card_b.getname]));
-        if card_a.GetScore > card_b.GetScore then begin
-            inc(a_score);
-            writeln('Point for player A');
+
+    while cards_remain do begin
+        if DEAL_FAIL in [card_pack.Deal(card_a), card_pack.Deal(card_b)] then begin
+            writeln('Deck is empty!');
+            cards_remain := false;
         end else begin
-            inc(b_score);
-            writeln('Point for player B');
+            write(format('cards drawn: A::%20s and B::%20s >> ', [card_a.getname, card_b.getname]));
+            if card_a.GetScore > card_b.GetScore then begin
+                inc(a_score);
+                writeln('Point for player A');
+            end else begin
+                inc(b_score);
+                writeln('Point for player B');
+            end;
         end;
     end;
     writeln(Format('Player A scored %d, player B scored %d', [a_score, b_score]));
